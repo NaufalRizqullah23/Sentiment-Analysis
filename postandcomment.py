ae5acc029ad352fdac7a931ddb8796843ac424a4
NaufalRizqullah23 <naufalrizqullah23@gmail.com>
2023-02-15 09:21:36 +0700

    now with amount of likes

--- postandcomment.py
+++ postandcomment.py
@@ -3,7 +3,7 @@ import requests
 import csv
 
 # Get your Access Token from Facebook Developer
-access_token = 'EAAR2De0jbC4BAKw2sHRZBzYqcR3ZAH95kZCX3Mir55PJhmVMrYXMAlVhDQJf4lSKYz9r0w2o5SBakwf4UrxHHz1HZAkEOKiVZBWx93J4YvZAsnYSDvdELyhQnI2t1pZCBg3kBKOwrGqCKf8SBKhnDtL6NC9pZA7U6EeXfgeIwOphFQDXv4mP0rVVJXlimJnNj2iU1Egp6QkPKhKUGDZAp25lS'
+access_token = 'EAAR2De0jbC4BANqcoEM5IhylB1GoKyikwhEDs4gOg2oHfUmrSzn4ylgkKjHMOBm2OuXYH35LzfCm3aUGxLdUOrTPTOvk8p4rGZBNYS1k8BlTKElpjRAYmlexDjhFVOpQF4pZBGgwNF95YuZCbqMjlekVZCZAZAtpfZC7OTPNFjLcuFtbK8Pjk47WHt86tWoVLIvotSyznEcPyGWkXTGTvxq'
 
 # Initialize the Facebook Graph API
 graph = facebook.GraphAPI(access_token)
@@ -15,19 +15,20 @@ page_id = '110397838639459'
 posts = graph.get_connections(id=page_id, connection_name='posts')
 
 # Create a CSV file
-with open('postscomments.csv', 'w', encoding='utf-8', newline='') as file:
+with open('withlikes.csv', 'w', encoding='utf-8', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Post ID', 'Post Message',
+    writer.writerow(['Post ID', 'Post Message', 'Likes',
                     'Comment ID', 'Comment Message'])
 
     # Loop through all the posts
     for post in posts['data']:
         post_id = post['id']
         post_message = post.get('message', '')
+        likes = post.get('likes', {}).get('summary', {}).get('total_count', 0)
         comments = graph.get_connections(
             id=post_id, connection_name='comments')
 
         # Loop through all the comments of the post
         for comment in comments['data']:
-            writer.writerow(
-                [post_id, post_message, comment['id'], comment['message']])
+            writer.writerow([post_id, post_message, likes,
+                            comment['id'], comment['message']])
