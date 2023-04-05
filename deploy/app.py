bd2b70c92fde49a71233f64243707cd1aa9ca0f8
NaufalRizqullah23 <naufalrizqullah23@gmail.com>
2023-04-05 13:57:52 +0700

    app code cleaning

--- deploy/app.py
+++ deploy/app.py
@@ -1,7 +1,4 @@
 import snscrape.modules.twitter as sntwitter
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-import nltk
 from joblib import load
 from flask import Flask, render_template, request
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
@@ -43,12 +40,6 @@ def predict():
 
     for i, tweet in enumerate(scraped_tweets):
         sentiment = model.predict(X[i])
-        # if sentiment[0] == -1:
-        #     sentiment[0] = 'NEGATIF'
-        # elif sentiment[0] == 0:
-        #     sentiment[0] = 'NETRAL'
-        # elif sentiment[0] == 1:
-        #     sentiment[0] = 'POSITIF'
         results.append((tweet, sentiment[0]))
 
         label_dict = {-1: 'NEGATIF', 0: 'NETRAL', 1: 'POSITIF'}
