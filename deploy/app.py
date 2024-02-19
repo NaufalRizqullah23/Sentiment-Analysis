a3c272016725a35811e9094ab4acda3d7e60bad0
Naufal Rizqullah <84897477+NaufalRizqullah23@users.noreply.github.com>
2024-02-19 21:27:35 +0700

    Finalization (Removed Login Detail)

--- deploy/app.py
+++ deploy/app.py
@@ -27,7 +27,7 @@ tw_api = API()
 
 
 async def add_and_log_in_accounts():
-    await tw_api.pool.add_account("Sinopla23", "Sandalbutut23.", "dajoka23@gmail.com", "Sandalbutut23.")
+    await tw_api.pool.add_account("TW_Username", "TW_Password", "TW_Email", "Email_Password")
     await tw_api.pool.login_all()
 
 
